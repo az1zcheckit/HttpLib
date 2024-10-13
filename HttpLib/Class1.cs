d01621dd4d84320eb92d62e39d6952f7b8c029f8
az1zcheckit <az1zcheckit@gmail.com>
2024-10-13 15:09:36 +0500

    news..

---
+++ HttpLib/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+
+namespace HttpRequestLibrary
+{
+    // Указываем COM-видимость библиотеки
+    [ComVisible(true)]
+    [Guid("01234567-89AB-CDEF-0123-456789ABCDEF")] // Уникальный идентификатор GUID
+    [ClassInterface(ClassInterfaceType.None)]
+    public class HttpHelper : IHttpHelper
+    {
+        // Метод для выполнения GET-запроса
+        public string SendGetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    response.EnsureSuccessStatusCode();
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+                catch (Exception ex)
+                {
+                    return string.Format("Error: {0}", ex.Message);
+                }
+            }
+        }
+
+        // Метод для выполнения POST-запроса
+        public string SendPostRequest(string url, string jsonData)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = client.PostAsync(url, content).Result;
+                    response.EnsureSuccessStatusCode();
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+                catch (Exception ex)
+                {
+                    return string.Format("Error: {0}", ex.Message);
+                }
+            }
+        }
+    }
+
+    // Интерфейс для реализации COM-взаимодействия
+    [ComVisible(true)]
+    [Guid("FEDCBA98-7654-3210-FEDC-BA9876543210")]
+    public interface IHttpHelper
+    {
+        string SendGetRequest(string url);
+        string SendPostRequest(string url, string jsonData);
+    }
+}
+
---
+++ HttpLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HttpLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HttpLib")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("63e8809e-6abb-46ec-ad23-9d478736b0d6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
